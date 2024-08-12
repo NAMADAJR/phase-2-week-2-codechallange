4ebb49776fbbb04a02cebb13af17cafbc6187c22
Namada Junior <namada871@gmail.com>
2024-08-12 22:52:06 +0300

    Adds and enlist function the enlists bots to the army

--- src/Components/BotCollection.js
+++ src/Components/BotCollection.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Bot from "./Bot";
+import BotArmy from "./BotArmy";
 
 const BotCollection = () => {
   const [bots, setBots] = useState([]);
+  const [enlistedBots, setEnlistedBots] = useState([]);
 
   useEffect(() => {
     fetch("http://localhost:3000/bots")
@@ -11,19 +13,29 @@ const BotCollection = () => {
       .catch((error) => console.log(error));
   }, []);
 
+  const enlistBot = (bot) => {
+    if (!enlistedBots.find((enlistedBot) => enlistedBot.id === bot.id)) {
+      setEnlistedBots([...enlistedBots, bot]);
+    }
+  };
+
   const displayBots = bots.map((bot) => (
     <div key={bot.id}>
-      <Bot bot={bot} />
+      <Bot bot={bot} enlistBot={() => enlistBot(bot)} />
     </div>
   ));
 
   return (
-    <>
+    <div>
       <div className="Title">
-        <h1>Bot Collection</h1>
+      <h2>Bot Collection</h2>
       </div>
       <div className="BotCollection">{displayBots}</div>
-    </>
+      <div className="Title">
+      <h2>Bot Army</h2>
+      </div>
+      <BotArmy bots={enlistedBots} />
+    </div>
   );
 };
 
