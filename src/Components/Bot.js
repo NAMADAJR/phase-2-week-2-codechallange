91b104aee78178d925253aee16d445548bf6e6ed
Namada Junior <namada871@gmail.com>
2024-08-12 19:17:17 +0300

    Formats data that has been fetches and arranges it

---
+++ src/Components/Bot.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Bot = ({ bot }) => {
+  return (
+    <div className="Bot">
+      <img src={bot.avatar_url} alt={bot.name} />
+      <h2>Bot name: {bot.name}</h2>
+      <h3>Bot Class: {bot.bot_class}</h3>
+      <h3>Health: {bot.health}</h3>
+      <h3>Damage: {bot.damage}</h3>
+      <h3>Armor: {bot.armor}</h3>
+    </div>
+  )
+}
+
+export default Bot
